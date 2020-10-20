88857885d152d3ff1dd4c3c44b14ee1b6de9747c
Defraene Michaël <micdef@hotmail.com>
2020-10-20 21:36:42 +0200

    Add project files.

---
+++ ToolBox/Logs/ILoggerService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToolBox.Logs
+{
+    /// <summary>
+    /// Service used to write in log files
+    /// </summary>
+    interface ILoggerService
+    {
+        /// <summary>
+        /// Method used to write in log with DEBUG level
+        /// </summary>
+        /// <param name="currentObjectName">Object name where caller is located</param>
+        /// <param name="callerName">Caller method</param>
+        /// <param name="message">Message to write</param>
+        /// <param name="parmeters">Parameters (variables, ...) It's a Dictionary with name and value</param>
+        void WriteDebug(string currentObjectName, string callerName, string message, Dictionary<string, string> parmeters);
+
+        /// <summary>
+        /// Method used to write in log with ERROR level
+        /// </summary>
+        /// <param name="currentObjectName">Object name where caller is located</param>
+        /// <param name="callerName">Caller method</param>
+        /// <param name="message">Message to write</param>
+        /// <param name="ex">Exception received by the caller (can be null)</param>
+        void WriteError(string currentObjectName, string callerName, string message, Exception ex = null);
+
+        /// <summary>
+        /// Method used to write in log with FATAL level
+        /// </summary>
+        /// <param name="currentObjectName">Object name where caller is located</param>
+        /// <param name="callerName">Caller method</param>
+        /// <param name="message">Message to write</param>
+        /// <param name="ex">Exception received by the caller (can be null)</param>
+        void WriteFatal(string currentObjectName, string callerName, string message, Exception ex = null);
+
+        /// <summary>
+        /// Method used to write in log with INFORMATION level
+        /// </summary>
+        /// <param name="currentObjectName">Object name where caller is located</param>
+        /// <param name="callerName">Caller method</param>
+        /// <param name="message">Message to write</param>
+        void WriteInformation(string currentObjectName, string callerName, string message);
+
+        /// <summary>
+        /// Method used to write in log with TRACE level
+        /// </summary>
+        /// <param name="currentObjectName">Object name where caller is located</param>
+        /// <param name="callerName">Caller method</param>
+        /// <param name="message">Message to write</param>
+        void WriteTrace(string currentObjectName, string callerName, string message);
+
+        /// <summary>
+        /// Method used to write in log with WARNING level
+        /// </summary>
+        /// <param name="currentObjectName">Object name where caller is located</param>
+        /// <param name="callerName">Caller method</param>
+        /// <param name="message">Message to write</param>
+        void WriteWarning(string currentObjectName, string callerName, string message);
+    }
+}
---
+++ ToolBox/Patterns/ServiceLocatorBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ToolBox.Patterns
+{
+    /// <summary>
+    /// Abstract class used to define the "Service Locator"'s class 
+    /// </summary>
+    public abstract class ServiceLocatorBase
+    {
+        /// <summary>
+        /// Property : Service container
+        /// </summary>
+        protected IServiceProvider Container { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ServiceLocatorBase()
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            Container = serviceCollection.BuildServiceProvider();
+        }
+
+        /// <summary>
+        /// Method use to define, register and configure available services
+        /// </summary>
+        /// <param name="serviceCollection">Collection where to add services</param>
+        public abstract void ConfigureServices(IServiceCollection serviceCollection);
+    }
+}
